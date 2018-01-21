4b7d914b7cdfadda9a57f62405d832487c8d2141
Sean Obringer <SeanObringer90@gmail.com>
2018-01-20 20:14:19 -0800

    verify token added

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,4 +1,5 @@
 const Model = require('../models/users-model.js')
+const jwt = require('jsonwebtoken')
 console.log('in controller');
 
 class UserController {
@@ -9,8 +10,37 @@ class UserController {
     })
   }
 
+  static verifyToken(req, res, next){
+    let [bearer, token] = req.headers.auth ? req.headers.auth.split(' ') : [null, null]
+    // console.log('bearer', bearer, 'token', token, 'header ', req.headers.auth);
+    // let signedToken = jwt.sign({
+    //   token: token
+    // }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+
+    console.log('vt token', signedToken);
+    jwt.verify(signedToken, process.env.TOKEN_SECRET, (err, verifiedToken) => {
+      if(err){
+        console.log('been an err ', err);
+        req.token = null
+      }
+      else {
+        console.log('no error');
+        req.token = token
+      }
+
+    })
+    res.status(200)
+    next()
+  }
+
+  static login(req, res, next){
+    console.log('header', req.headers.auth);
+    console.log('body: ',req.body);
+    res.status(201)
+  }
+
 }
-  
+
 
 
 
--- routes/users-router.js
+++ routes/users-router.js
@@ -3,6 +3,7 @@ const Controller = require('../controllers/users-controller.js')
 console.log('user router');
 
 router.get('/', Controller.getAllUsers)
+router.post('/login', Controller.verifyToken, Controller.login)
 
 
 module.exports = router;
